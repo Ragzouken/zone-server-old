f2f36a5791c3385f896c20d0925c316666f0e3a2
mark <ragzouken@gmail.com>
2020-04-12 15:59:06 +0100

    whoops

---
+++ src/utility.ts
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch';
+import * as HTMLParser from 'node-html-parser';
+
+export function copy<T>(object: T) {
+    return JSON.parse(JSON.stringify(object));
+}
+
+export function timeToSeconds(time: string): number {
+    const parts = time.split(':');
+
+    const seconds = parseInt(parts.pop() || '0', 10);
+    const minutes = parseInt(parts.pop() || '0', 10);
+    const hours = parseInt(parts.pop() || '0', 10);
+
+    return seconds + minutes * 60 + hours * 3600;
+}
+
+export async function fetchDom(url: string): Promise<HTMLParser.HTMLElement> {
+    const address = encodeURI(url);
+    const response = await fetch(address);
+    const html = await response.text();
+    return HTMLParser.parse(html) as HTMLParser.HTMLElement;
+}
